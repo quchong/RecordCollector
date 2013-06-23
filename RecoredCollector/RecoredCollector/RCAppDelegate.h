37b414b44dfa01d8998b5232ed4a56eca386b184
quchong <quchong1986@gmail.com>
2013-06-23 15:36:10 +0800

    new files

---
+++ RecoredCollector/RecoredCollector/RCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RCAppDelegate.h
+//  RecoredCollector
+//
+//  Created by quchong on 13-6-23.
+//  Copyright (c) 2013年 com.quchong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RecoredCollector/RecoredCollector/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RecoredCollector
+//
+//  Created by quchong on 13-6-23.
+//  Copyright (c) 2013年 com.quchong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RCAppDelegate class]));
+    }
+}
